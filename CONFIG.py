db1d2b2bc2474c5d9df75f498928c378161bb92c
mahdevar <mahdevar@kashanu.ac.ir>
2024-10-22 16:07:03 +0330

    پیکربندی کنونی.

---
+++ CONFIG.py
@@ -0,0 +1,39 @@
+SQL = \
+{
+	'host': '192.168.10.31',
+	'user': 'developer_user',
+	'password': 'yn1FtrD12793U8x8UnPD',
+	'database': 'a'
+}
+
+REDIS = \
+{
+	'host': '192.168.10.31',
+	'port': 6379,
+	'password': 'fgh3Y9mwXglk5VP3783L',
+	'db': 5
+}
+
+MINIO = \
+{
+	'endpoint': '192.168.10.31:9000',
+	'access_key': 'ACCESSMINIOMINIO',
+	'secret_key': 'SECRETMINIOMINIO',
+	'secure': False
+}
+
+MONGO = \
+{
+	'host': 'mongodb://developer_user:1YZ9o6GF0JMJ850Niv28@192.168.10.31:27017'
+}
+
+
+CSP_DIRECTIVES = \
+{
+	'connect-src': ['geolocation-db.com'],
+	'font-src': ['fonts.gstatic.com'],
+	'frame-ancestors': [],
+	'img-src': [],
+	'script-src': [],
+	'style-src': ['fonts.googleapis.com']
+}
